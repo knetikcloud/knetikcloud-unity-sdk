e4a0cca53a22bb3c25bbf4ad2ffe5e9f906dfddb
George Hornmoen <george.hornmoen@gmail.com>
2017-09-14 00:46:23 -0700

    Enable/disable menu items based on the URL being set.  Now links to the developers' space and not the default sandbox.

--- Assets/ThirdParty/KnetikCloud/Editor/KnetikEditorExtensions.cs
+++ Assets/ThirdParty/KnetikCloud/Editor/KnetikEditorExtensions.cs
@@ -1,27 +1,68 @@
 ﻿using UnityEngine;
 using UnityEditor;
-
+using com.knetikcloud.Client;
 
 namespace com.knetikcloud.UnityEditor
 {
     public static class KnetikEditorExtensions
     {
+        public const string GettingStartedMenuItem = "Knetik Cloud/Getting Started...";
+        public const string ApiDocsMenuItem = "Knetik Cloud/API Docs...";
+
+        public static KnetikProjectSettings ProjectSettings;
+
+        static KnetikEditorExtensions()
+        {
+            LoadProjectSettings();
+        }
+
+        public static void LoadProjectSettings()
+        {
+            if (ProjectSettings == null)
+            {
+                ProjectSettings = KnetikEditorScriptableObjectUtis.LoadPersistentData<KnetikProjectSettings>(KnetikProjectSettings.SaveDataPath);
+                if (ProjectSettings == null)
+                {
+                    ProjectSettings = ScriptableObject.CreateInstance<KnetikProjectSettings>();
+                    KnetikEditorAssetDatabaseUtils.CreateAssetAndDirectories(ProjectSettings, KnetikProjectSettings.SaveDataPath);
+                }
+            }
+        }
+
         [MenuItem("Knetik Cloud/Sign up...")]
         private static void SignUp()
         {
             Application.OpenURL("http://knetikcloud.com/");
         }
 
-        [MenuItem("Knetik Cloud/Getting Started...")]
+        [MenuItem(GettingStartedMenuItem)]
         private static void GettingStarted()
         {
-            Application.OpenURL("https://sandbox.knetikcloud.com/index.html");
+            if ((ProjectSettings != null) && (!string.IsNullOrEmpty(ProjectSettings.BaseUrl)))
+            {
+                Application.OpenURL(ProjectSettings.BaseUrl);
+            }
         }
 
-        [MenuItem("Knetik Cloud/API Docs...")]
-        private static void APIDocs()
+        [MenuItem(GettingStartedMenuItem, true)]
+        private static bool CheckGettingStarted()
         {
-            Application.OpenURL("https://sandbox.knetikcloud.com/api.html");
+            return ((ProjectSettings != null) && (!string.IsNullOrEmpty(ProjectSettings.BaseUrl)));
+        }
+
+        [MenuItem(ApiDocsMenuItem)]
+        private static void ApiDocs()
+        {
+            if ((ProjectSettings != null) && (!string.IsNullOrEmpty(ProjectSettings.BaseUrl)))
+            {
+                Application.OpenURL(ProjectSettings.BaseUrl + "/api.html");
+            }
+        }
+
+        [MenuItem(ApiDocsMenuItem, true)]
+        private static bool CheckApiDocs()
+        {
+            return ((ProjectSettings != null) && (!string.IsNullOrEmpty(ProjectSettings.BaseUrl)));
         }
     }
 }
